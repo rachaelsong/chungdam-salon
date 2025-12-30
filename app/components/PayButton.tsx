73afc0efe4ff37b65a9b61aaf64024f9e59d4bee
rachaelsong <heewons38@gmail.com>
2025-12-30 15:29:05 +0900

    Update PayButton.tsx

--- app/components/PayButton.tsx
+++ app/components/PayButton.tsx
@@ -6,8 +6,14 @@ export type PayButtonProps = {
 };
 
 export default function PayButton({ planName, amount }: PayButtonProps) {
+  // TODO: 결제 페이지 이동/결제 로직은 다음 단계에서 연결
+  const onClick = () => {
+    // 임시: 빌드 에러 제거용
+    alert(`${planName} / ${amount.toLocaleString()}원 결제 연결 예정`);
+  };
+
   return (
-    <button type="button" className="cs-btn cs-btn-full">
+    <button type="button" className="cs-btn cs-btn-full" onClick={onClick}>
       {planName} 결제하기 ({amount.toLocaleString()}원)
     </button>
   );
