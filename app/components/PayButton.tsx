450b5e0d08bb92ed2e413089d83fc4ddf54196c6
rachaelsong <heewons38@gmail.com>
2025-12-30 14:50:37 +0900

    Update PayButton.tsx

--- app/components/PayButton.tsx
+++ app/components/PayButton.tsx
@@ -6,12 +6,8 @@ type PayButtonProps = {
 };
 
 export default function PayButton({ planName, amount }: PayButtonProps) {
-  const onClick = () => {
-    alert(`${planName} / ${amount.toLocaleString()}원 결제 로직 연결 필요`);
-  };
-
   return (
-    <button type="button" onClick={onClick}>
+    <button type="button">
       {planName} 결제 ({amount.toLocaleString()}원)
     </button>
   );
